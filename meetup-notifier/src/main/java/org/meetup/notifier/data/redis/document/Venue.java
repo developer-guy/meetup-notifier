5b046e325a0ebf15e3894b9ed6d4491990144a6c
Batuhan Apaydın <developerguy2@gmail.com>
2018-06-12 13:09:15 +0300

    document,repository created

--- meetup-notifier/src/main/java/org/meetup/notifier/data/redis/document/Venue.java
+++ meetup-notifier/src/main/java/org/meetup/notifier/data/redis/document/Venue.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Objects;
+
 /**
  * @author Batuhan Apaydın
  */
@@ -21,4 +23,27 @@ public class Venue {
     private String city;
     private String country;
     private String localizedCountryName;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Venue venue = (Venue) o;
+        return isRePinned() == venue.isRePinned() &&
+                Objects.equals(getId(), venue.getId()) &&
+                Objects.equals(getName(), venue.getName()) &&
+                Objects.equals(getLatitude(), venue.getLatitude()) &&
+                Objects.equals(getLongitude(), venue.getLongitude()) &&
+                Objects.equals(getAddress1(), venue.getAddress1()) &&
+                Objects.equals(getAddress2(), venue.getAddress2()) &&
+                Objects.equals(getCity(), venue.getCity()) &&
+                Objects.equals(getCountry(), venue.getCountry()) &&
+                Objects.equals(getLocalizedCountryName(), venue.getLocalizedCountryName());
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(getId(), getName(), getLatitude(), getLongitude(), isRePinned(), getAddress1(), getAddress2(), getCity(), getCountry(), getLocalizedCountryName());
+    }
 }
