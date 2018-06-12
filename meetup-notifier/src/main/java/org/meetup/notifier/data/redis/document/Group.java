fc656db542fc1658feb89f4a675d0175fcae98ec
Batuhan Apaydın <developerguy2@gmail.com>
2018-06-12 13:09:15 +0300

    daaaaaaaaadasdasdasdasdadasocument,repository created

--- meetup-notifier/src/main/java/org/meetup/notifier/data/redis/document/Group.java
+++ meetup-notifier/src/main/java/org/meetup/notifier/data/redis/document/Group.java
@@ -5,6 +5,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.meetup.notifier.client.dto.JoinMode;
 
+import java.util.Objects;
+
 /**
  * @author Batuhan Apaydın
  */
@@ -22,4 +24,27 @@ public class Group {
     private String who;
     private String localizedLocation;
     private String region;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Group group = (Group) o;
+        return Objects.equals(getCreated(), group.getCreated()) &&
+                Objects.equals(getName(), group.getName()) &&
+                Objects.equals(getId(), group.getId()) &&
+                getJoinMode() == group.getJoinMode() &&
+                Objects.equals(getLatitude(), group.getLatitude()) &&
+                Objects.equals(getLongitude(), group.getLongitude()) &&
+                Objects.equals(getUrlName(), group.getUrlName()) &&
+                Objects.equals(getWho(), group.getWho()) &&
+                Objects.equals(getLocalizedLocation(), group.getLocalizedLocation()) &&
+                Objects.equals(getRegion(), group.getRegion());
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(getCreated(), getName(), getId(), getJoinMode(), getLatitude(), getLongitude(), getUrlName(), getWho(), getLocalizedLocation(), getRegion());
+    }
 }
